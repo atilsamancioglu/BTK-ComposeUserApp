18a7363ef17c685b4a38107f48851921ecc0e13c
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-11-15 11:36:23 +0300

    service added

---
+++ app/src/main/java/com/atilsamancioglu/userapp/service/UserAPI.kt
@@ -0,0 +1,13 @@
+package com.atilsamancioglu.userapp.service
+
+import com.atilsamancioglu.userapp.model.User
+import retrofit2.http.GET
+
+interface UserAPI {
+
+    //source -> https://jsonplaceholder.typicode.com/users
+
+    @GET("atilsamancioglu/UsersJSONPlaceHolder/refs/heads/main/users.json")
+    suspend fun getData(): List<User>
+
+}
