9f8608b9f78e5411225749d00d8d48765786242d
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-11-15 11:40:37 +0300

    screen added

--- app/src/main/java/com/atilsamancioglu/userapp/screens/UserScreen.kt
+++ app/src/main/java/com/atilsamancioglu/userapp/screens/UserScreen.kt
@@ -1,2 +1,74 @@
 package com.atilsamancioglu.userapp.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.atilsamancioglu.userapp.model.User
+
+@Composable
+fun JokeList(userList: List<User>) {
+    LazyColumn(modifier = Modifier
+        .fillMaxSize()
+        .background(color = MaterialTheme.colorScheme.primaryContainer)
+    ) {
+        items(userList) {
+            UserRow(user = it)
+        }
+    }
+
+}
+
+
+@Composable
+fun UserRow(user: User) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .border(BorderStroke(2.dp, Color.Black)) // Adding a border here
+
+        .background(color = MaterialTheme.colorScheme.primaryContainer)
+        .padding(10.dp)
+    ) {
+
+            Column {
+                Text(text= (user.name),
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.padding(2.dp),
+                    fontWeight = FontWeight.Normal
+                )
+
+                Text(text= (user.email),
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.padding(2.dp),
+                    fontWeight = FontWeight.Normal
+                )
+
+                Text(text= (user.website),
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.padding(2.dp),
+                    fontWeight = FontWeight.Normal
+                )
+
+                Text(text= (user.phone),
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.padding(2.dp),
+                    fontWeight = FontWeight.Normal
+                )
+
+            }
+
+
+    }
+}
